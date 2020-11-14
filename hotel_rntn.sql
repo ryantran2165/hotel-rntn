fb7df0eb8a023851deb481b0d382b6e38d72fc43
ryan_tran <ryantran2165@gmail.com>
2020-11-14 14:57:12 -0800

    Make email unique

--- hotel_rntn.sql
+++ hotel_rntn.sql
@@ -5,7 +5,7 @@ USE hotel_rntn;
 CREATE TABLE account (
   PRIMARY KEY (id),
   id         MEDIUMINT UNSIGNED AUTO_INCREMENT,
-  email      VARCHAR(255) NOT NULL,
+  email      VARCHAR(255) NOT NULL UNIQUE,
   password   VARCHAR(32)  NOT NULL,
   first_name VARCHAR(255) NOT NULL,
   last_name  VARCHAR(255) NOT NULL,
