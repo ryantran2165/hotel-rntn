dccee726013c97d3f36d646cb968a6ee9dd32547
Tien-PC <Tien.Nguyen072399@gmail.com>
2020-11-14 15:13:05 -0800

    Implemented User Sign up, User Login, Admin Login

--- HotelRNTN.java
+++ HotelRNTN.java
@@ -6,7 +6,7 @@ import java.util.Scanner;
 public class HotelRNTN {
 	private static final String DATABASE_NAME = "hotel_rntn";
 	private static final String DATABASE_USERNAME = "root";
-	private static final String DATABASE_PASSWORD = "toor";
+	private static final String DATABASE_PASSWORD = "ss3goku90";
 
 	public static void main(String[] args) {
 		try {
@@ -19,26 +19,89 @@ public class HotelRNTN {
 
 				String choice = "";
 				String[] options = { "1", "2", "3" };
+				String status = "main";
 
-				while (true) {
-					System.out.printf("Please choose an option:%5s%s%5s%s%5s%s%n", "",
-							createOptionStr(options[0], "User"), "", createOptionStr(options[1], "Admin"), "",
-							createOptionStr(options[2], "Quit"));
-					choice = scanner.nextLine();
+				while (status != "exit") {
+					//Main Menu
+					if(status == "main") {
+						System.out.printf("Please choose an option:%5s%s%5s%s%5s%s%n", "",
+								createOptionStr(options[0], "User"), "", createOptionStr(options[1], "Admin"), "",
+								createOptionStr(options[2], "Quit"));
+						choice = scanner.nextLine();
 
-					if (choice.equals(options[0])) {
-						new Guest(conn);
-					} else if (choice.equals(options[1])) {
-						new Manager(conn);
-					} else if (choice.equals(options[2])) {
-						System.out.println("Thank you for visiting Hotel RNTN!");
-						System.exit(0);
-					} else {
-						System.out.println("Invalid choice, please try again!");
+						if (choice.equals(options[0])) {
+							status = "guest";
+						} else if (choice.equals(options[1])) {
+							status = "admin";
+
+						} else if (choice.equals(options[2])) {
+							System.out.println("Thank you for visiting Hotel RNTN!");
+							System.exit(0);
+						} else {
+							System.out.println("Invalid choice, please try again!");
+
+						}
+					}
+
+					//User / Guest menu
+					else if(status == "guest") {
+						Guest g = new Guest(conn);
+						System.out.println("Guest Portal");
+						System.out.printf("User Options:%5s%s%5s%s%5s%s%n", "",
+								createOptionStr(options[0], "Sign Up"), "", createOptionStr(options[1], "Login"), "",
+								createOptionStr(options[2], "Back"));
+						choice = scanner.nextLine();
+
+						//Sign up
+						if(choice.contentEquals(options[0])) {
+							System.out.println(g.signUp());
+						}
+
+						//Login
+						else if(choice.contentEquals(options[1])) {
+							//Success = true if all the credentials match
+							boolean success = g.login();
+
+							if(success) {
+								System.out.println("You have logged in as a Guest!\n");
+							}
+							else {
+								System.out.println("Incorrect email or password\n");
+							}
+						}
+						else if(choice.contentEquals(options[2])) {
+							status = "main";
+						}
+
+					}
+
+					//Manager Menu
+					else if(status == "admin") {
+						Manager m = new Manager(conn);
+						Guest g = new Guest(conn);
+						System.out.println("Manager Portal");
+						System.out.printf("User Options:%5s%s%5s%s%n", "",
+								createOptionStr(options[0], "Login"), "",
+								createOptionStr(options[1], "Back"));
+						choice = scanner.nextLine();
+						if(choice.contentEquals(options[0])) {
+							boolean success = m.login();
+							if(success) {
+								System.out.println("You have logged in as a Manager!\n");
+							}
+							else {
+								System.out.println("Incorrect email or password\n");
+							}
+						}
+						else if(choice.contentEquals(options[1])) {
+							status = "main";					
+						}
 					}
 				}
 			}
 		} catch (SQLException e) {
+
+			System.out.println(e);
 			System.out.println("Connection to database failed!");
 		}
 	}
